6401fe32e7af626a239ad0e04f2d8ec4055d1058
Ananya Gupta <anannya.gta@gmail.com>
2024-04-19 15:16:49 +0530

    Added new files

--- Cards/main.go
+++ Cards/main.go
@@ -1,10 +1,17 @@
+// Note: files in the same package do not have to be imported into each other
+
 package main
 
 import "fmt"
 
 func main() {
 	// Go is a static type of programming language, you nee dto specify data types to variables just like java
-	// var card string = "Ace of Hearts"
-	card := "Ace of Hearts" // use colon only for very first initialization
+	// var card string = "Ace of Hearts" or use card := "Ace of Hearts"
+	// use colon only for very first initialization
+	card := newCard() 
 	fmt.Println("Card: ", card)
+}
+
+func newCard() string{
+	return "Five of Diamonds"
 }
