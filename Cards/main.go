e41e3f981215025c843b4bbd4cadf888c00e7df2
Ananya Gupta <anannya.gta@gmail.com>
2024-04-19 15:25:16 +0530

    Arrays, Slices, For Loops

--- Cards/main.go
+++ Cards/main.go
@@ -4,14 +4,34 @@ package main
 
 import "fmt"
 
+/*
 func main() {
-	// Go is a static type of programming language, you nee dto specify data types to variables just like java
+	// Go is a static type of programming language, you need to specify data types to variables just like java
 	// var card string = "Ace of Hearts" or use card := "Ace of Hearts"
 	// use colon only for very first initialization
-	card := newCard() 
+	card := newCard()
 	fmt.Println("Card: ", card)
 }
 
 func newCard() string{
 	return "Five of Diamonds"
+} */
+
+// Array: Fixed Length list of things of same data type
+// Slice: An array that can grow or shrink, length is not fixed
+
+func main() {
+	cards := []string{"Ace of Hearts", newCard()}
+	cards = append(cards, "Five of Diamonds") //slice
+
+	// iterating over elements
+	for i, card := range cards {
+		fmt.Println(i, card)
+	}
+
+}
+
+func newCard() string {
+	return "Six of Spades"
+
 }
