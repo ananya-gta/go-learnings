b6dc538494e39b27d85816f6b55f35e029e976e2
Ananya Gupta <anannya.gta@gmail.com>
2024-04-19 15:02:46 +0530

    Added new files

---
+++ Cards/main.go
@@ -0,0 +1,10 @@
+package main
+
+import "fmt"
+
+func main() {
+	// Go is a static type of programming language, you nee dto specify data types to variables just like java
+	// var card string = "Ace of Hearts"
+	card := "Ace of Hearts" // use colon only for very first initialization
+	fmt.Println("Card: ", card)
+}
