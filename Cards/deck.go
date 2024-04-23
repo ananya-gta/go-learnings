4b6043a963be09354424149f8ca0957e54f3a9b5
Ananya Gupta <anannya.gta@gmail.com>
2024-04-23 11:34:37 +0530

    added new functions - "toString" and "saveToFile"

--- Cards/deck.go
+++ Cards/deck.go
@@ -36,3 +36,12 @@ func (d deck) print() {
 func deal(d deck, handSize int) (deck, deck) {
 	return d[:handSize], d[handSize:]
 }
+
+
+func (d deck) toString() string {
+	return strings.Join([]string(d), ",")
+}
+
+func (d deck) saveToFile(filename string) error {
+  return os.WriteFile(filename, []byte(d.toString()), 0666)
+}
